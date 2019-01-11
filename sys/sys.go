23e110817556fcdf066f8d239c977ca03315cfa0
Michel Courtine <github@michak.net>
2019-01-11 21:38:13 +0100

    cleaned up sys.go
    Signed-off-by: Michel Courtine <github@michak.net>

--- sys/sys.go
+++ sys/sys.go
@@ -5,18 +5,13 @@ import (
 )
 
 func initLogger() {
-	Formatter := new(log.TextFormatter)
-	Formatter.TimestampFormat = "02-01-2006 15:04:05"
-	Formatter.FullTimestamp = true
-	log.SetFormatter(Formatter)
+	formatter := new(log.TextFormatter)
+	formatter.TimestampFormat = "02-01-2006 15:04:05"
+	formatter.FullTimestamp = true
+	log.SetFormatter(formatter)
 }
 
 func Init() {
 	log.Info("Initializing...")
-	// log.Info("Some info. Earth is not flat.")
-	// log.Warning("This is a warning")
-	// log.Error("Not fatal. An error. Won't stop execution")
-	// log.Fatal("MAYDAY MAYDAY MAYDAY. Execution will be stopped here")
-	// log.Panic("Do not panic")
 	initLogger()
 }
