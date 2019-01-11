4e93684bd3f324599f66d4d2c22fa5073d4220b3
Michel Courtine <github@michak.net>
2019-01-12 00:10:30 +0100

    cosmetic fixes
    Signed-off-by: Michel Courtine <github@michak.net>

--- cmd/build.go
+++ cmd/build.go
@@ -15,8 +15,7 @@
 package cmd
 
 import (
-	"fmt"
-
+	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -30,9 +29,11 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("build called")
-	},
+	Run: run,
+}
+
+func run(cmd *cobra.Command, args []string) {
+	log.Info("Building")
 }
 
 func init() {
--- cmd/deps.go
+++ cmd/deps.go
@@ -15,24 +15,25 @@
 package cmd
 
 import (
-	"fmt"
-
+	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 // depsCmd represents the deps command
 var depsCmd = &cobra.Command{
 	Use:   "deps",
-	Short: "A brief description of your command",
+	Short: "Installs system dependencies",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deps called")
-	},
+	Run: run,
+}
+
+func run(cmd *cobra.Command, args []string) {
+	log.Info("Installing dependencies")
 }
 
 func init() {
