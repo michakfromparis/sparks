5f2257f9bb9a84db6ab390df8d3c50fb9c90ee0f
Michel Courtine <github@michak.net>
2019-01-13 22:40:34 +0100

    build fix
    Signed-off-by: Michel Courtine <github@michak.net>

--- sparks/sparks.go
+++ sparks/sparks.go
@@ -1,29 +1,53 @@
 package sparks
 
 import (
+	"os"
+
+	log "github.com/Sirupsen/logrus"
 	"github.com/michaKFromParis/sparks/config"
 	"github.com/michaKFromParis/sparks/errx"
 )
 
 func Init() {
+	log.Info("sparks init")
 	if err := config.Init(); err != nil {
-		errx.FatalF(err, "Configuration initialization failed")
+		errx.Fatalf(err, "Configuration initialization failed")
 	}
 }
 
 func Shutdown() {
 }
 
-func LoadProjectConfig() {
+func Load() {
+	log.Info("sparks load")
+	log.Tracef("opening %s", config.SourceDirectory)
+	f, err := os.Open(config.SourceDirectory)
+	if err != nil {
+		errx.Fatalf(err, "Could not open SourceDirectory: "+config.SourceDirectory)
+	}
+	files, err := f.Readdir(-1)
+	if err != nil {
+		errx.Fatalf(err, "Could not read SourceDirectory: "+config.SourceDirectory)
+	}
+	if err = f.Close(); err != nil {
+		errx.Fatalf(err, "Could not close SourceDirectory: "+config.SourceDirectory)
+	}
+	log.Trace("files in SourceDirectory:")
+	for _, file := range files {
+		log.Trace(file.Name())
+	}
 }
 
 func Build() {
+	log.Info("sparks build")
+	Load()
 	for _, platformName := range PlatformNames {
 		platform := Platforms[platformName]
 		if platform != nil && platform.Enabled() {
 			for _, configurationName := range ConfigurationNames {
 				configuration := Configurations[configurationName]
 				if configuration != nil && configuration.Enabled() {
+					Load()
 					platform.Build(configuration)
 				}
 			}
