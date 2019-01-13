c388f6b6cda79b7b71e2ca73283642557d7a8a10
Michel Courtine <github@michak.net>
2019-01-13 22:31:47 +0100

    failsafe(r) config
    Signed-off-by: Michel Courtine <github@michak.net>

--- config/config.go
+++ config/config.go
@@ -5,40 +5,43 @@ import (
 	"path"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/michaKFromParis/sparks/errx"
 	"github.com/michaKFromParis/sparks/utils"
 )
 
 var SourceDirectory string
 var OutputDirectory string
 var ProductName string
-var Platforms map[string]bool
 var Debug = false
 var Release = false
 var Shipping = false
 
+var Platforms map[string]bool
+var Verbose = false
+
 func Init() error {
-	log.Info("Initializing config")
+	log.Info("initializing config")
 	ProductName = "Sparks"
-	SourceDirectory = utils.Pwd()
+
+	var err error
+	if SourceDirectory, err = utils.Pwd(); err != nil {
+		errx.FatalF(err, "could not determine current working directory")
+	}
 	OutputDirectory = path.Join(SourceDirectory, "build")
 	Platforms = make(map[string]bool)
+	log.Debug(String())
 	return nil
 }
 
 func String() string {
-	// platforms := ""
-	// for _, name := range platform.PlatformNames {
-	// 	if Platforms[name] {
-	// 		platforms += name + " "
-	// 	}
-	// }
-	return fmt.Sprintf(
-		`Loaded Configuration:
+	return fmt.Sprintf(`Loaded Configuration:
 ProductName: %s
 SourceDirectory: %s
 OutputDirectory: %s
 Debug: %t
 Release: %t
-Shipping: %t`, ProductName, SourceDirectory, OutputDirectory, Debug, Release, Shipping)
-	// Platforms: %s`, ProductName, SourceDirectory, OutputDirectory, Debug, Release, Shipping, platforms)
+Shipping: %t
+Verbose: %t
+Platforms: %s`, ProductName, SourceDirectory, OutputDirectory, Debug, Release, Shipping, Verbose, platforms)
+
 }
