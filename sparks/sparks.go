3d0379a852478cf477d452f44c7a39f8d3ce2f8e
Michel Courtine <github@michak.net>
2019-01-14 14:18:28 +0100

    creating build output directory structure
    Signed-off-by: Michel Courtine <github@michak.net>

--- sparks/sparks.go
+++ sparks/sparks.go
@@ -1,9 +1,13 @@
 package sparks
 
 import (
+	"os"
+	"path/filepath"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/michaKFromParis/sparks/config"
 	"github.com/michaKFromParis/sparks/errx"
+	"github.com/michaKFromParis/sparks/utils"
 )
 
 var CurrentProduct = Product{}
@@ -28,10 +32,32 @@ func Save() {
 	log.Info("sparks save")
 	CurrentProduct.Save()
 }
+func createBuildDirectoryStructure() {
+	log.Trace("creating build/bin, build/lib, build/projects")
+	var binPath = filepath.Join(config.OutputDirectory, "bin")
+	var libPath = filepath.Join(config.OutputDirectory, "lib")
+	var projectsPath = filepath.Join(config.OutputDirectory, "projects")
+	if err := os.MkdirAll(binPath, os.ModePerm); err != nil {
+		errx.Error("failed to create build bin directory: " + binPath)
+	}
+	if err := os.MkdirAll(libPath, os.ModePerm); err != nil {
+		errx.Error("failed to create build lib directory: " + libPath)
+	}
+	if err := os.MkdirAll(projectsPath, os.ModePerm); err != nil {
+		errx.Error("failed to create build projects directory: " + projectsPath)
+	}
+}
+
+func generateLuaBindings() {
+	
+	utils.Execute("ls", "-la")
+}
 
 func Build() {
 	log.Info("sparks build")
 	Load()
+	createBuildDirectoryStructure()
+	generateLuaBindings()
 	for _, platformName := range PlatformNames {
 		platform := Platforms[platformName]
 		if platform != nil && platform.Enabled() {
