a2f24e96e2f5cd3796aae4c8a351d8a397cc3097
Michel Courtine <github@michak.net>
2019-01-17 19:56:43 +0100

    added piped commands execution
    Signed-off-by: Michel Courtine <github@michak.net>

--- utils/exec.go
+++ utils/exec.go
@@ -3,6 +3,7 @@
 package utils
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -87,4 +88,102 @@ func writer(r io.Reader) {
 			break
 		}
 	}
+}
+
+// Convert a shell command with a series of pipes into
+// correspondingly piped list of *exec.Cmd
+// If an arg has spaces, this will fail
+func ExecutePipe(s string) (string, error) {
+	buf := bytes.NewBuffer([]byte{})
+	sp := strings.Split(s, "|")
+	cmds := make([]*exec.Cmd, len(sp))
+	// create the commands
+	for i, c := range sp {
+		cs := strings.Split(strings.TrimSpace(c), " ")
+		cmd := cmdFromString(cs)
+		cmds[i] = cmd
+	}
+
+	cmds = assemblePipes(cmds, nil, buf)
+	if err := runCmds(cmds); err != nil {
+		return "", err
+	}
+
+	b := buf.Bytes()
+	return string(b), nil
+}
+
+func cmdFromString(cs []string) *exec.Cmd {
+	if len(cs) == 1 {
+		return exec.Command(cs[0])
+	} else if len(cs) == 2 {
+		return exec.Command(cs[0], cs[1])
+	}
+	return exec.Command(cs[0], cs[1:]...)
+}
+
+// Convert sequence of tokens into commands,
+// using "|" as a delimiter
+func ExecutePipes(tokens ...string) (string, error) {
+	if len(tokens) == 0 {
+		return "", nil
+	}
+	buf := bytes.NewBuffer([]byte{})
+	cmds := []*exec.Cmd{}
+	args := []string{}
+	// accumulate tokens until a |
+	for _, t := range tokens {
+		if t != "|" {
+			args = append(args, t)
+		} else {
+			cmds = append(cmds, cmdFromString(args))
+			args = []string{}
+		}
+	}
+	cmds = append(cmds, cmdFromString(args))
+	cmds = assemblePipes(cmds, nil, buf)
+	if err := runCmds(cmds); err != nil {
+		return "", fmt.Errorf("%s; %s", err.Error(), string(buf.Bytes()))
+	}
+
+	b := buf.Bytes()
+	return string(b), nil
+}
+
+// Pipe stdout of each command into stdin of next
+func assemblePipes(cmds []*exec.Cmd, stdin io.Reader, stdout io.Writer) []*exec.Cmd {
+	cmds[0].Stdin = stdin
+	cmds[0].Stderr = stdout
+	// assemble pipes
+	for i, c := range cmds {
+		if i < len(cmds)-1 {
+			cmds[i+1].Stdin, _ = c.StdoutPipe()
+			cmds[i+1].Stderr = stdout
+		} else {
+			c.Stdout = stdout
+			c.Stderr = stdout
+		}
+	}
+	return cmds
+}
+
+// Run series of piped commands
+func runCmds(cmds []*exec.Cmd) error {
+	// start processes in descending order
+	for i := len(cmds) - 1; i > 0; i-- {
+		if err := cmds[i].Start(); err != nil {
+			return err
+		}
+	}
+	// run the first process
+	if err := cmds[0].Run(); err != nil {
+		return err
+	}
+	// wait on processes in ascending order
+	for i := 1; i < len(cmds); i++ {
+		if err := cmds[i].Wait(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
