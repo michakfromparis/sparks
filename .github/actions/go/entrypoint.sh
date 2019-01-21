1e1f887d2891bc14963b7420ddcffbbd70c46039
Michel Courtine <github@michak.net>
2019-01-21 19:40:23 +0100

    added a go action
    Signed-off-by: Michel Courtine <github@michak.net>

---
+++ .github/actions/go/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -e
+
+APP_DIR="/go/src/github.com/${GITHUB_REPOSITORY}/"
+
+mkdir -p ${APP_DIR} && cp -r ./ ${APP_DIR} && cd ${APP_DIR}
+
+if [[ "$1" == "format" ]]; then
+    echo "Running go format "
+    files=$(gofmt -l $(find . -type f -name '*.go' -not -path "./vendor/*") 2>&1)
+    if [ "$files" ]; then
+      echo "These files did not pass the gofmt checks:"
+      echo ${files}
+      exit 1
+    fi
+fi
+
+if [[ "$1" == "test" ]]; then
+    echo "Running go test"
+    go get ./...
+    go test $(go list ./... | grep -v /vendor/) -race -coverprofile=coverage.txt -covermode=atomic
+    cat coverage.txt
+fi
+
+if [[ "$1" == "build" ]]; then
+    echo "Running go build"
+    go get ./...
+    go build $(go list ./... | grep -v /vendor/)
+fi
+
