50c06e4250a707f5d858efd06427d5ff1ad547cc
Michel Courtine <github@michak.net>
2019-01-22 06:25:06 +0100

    trivial change to test actions
    Signed-off-by: Michel Courtine <github@michak.net>

--- platform/linux.go
+++ platform/linux.go
@@ -128,6 +128,7 @@ func (l *Linux) generate() {
 }
 
 func (l *Linux) compile() {
+	
 	log.Info("sparks project compile --linux")
 	projectsPath := filepath.Join(config.OutputDirectory, "projects", l.Title()+"-"+l.configuration.Title())
 	out, err := sys.ExecuteEx("make", projectsPath, true, "-j8")
